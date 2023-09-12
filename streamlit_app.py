5ffc985752244a6d11f230c0df94146b413e24e8
nguyenkimlong <nguyenkimlong95@gmail.com>
2023-09-12 17:21:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,6 @@ if st.button('cal'):
     st.subheader("longnk- click button")
 
 
-if __name__ == "__main__":
-   st.subheader("longnk- __main__")
-
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
